eeb7478b01f62425d301bb813a240f93e9233482
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-02-06 22:17:48 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 "use strict";
+// class User {
+//     public email:string
+//     private name:string
+//     readonly city:string="harayana"      //In TypeScript, a constructor is a special method of a class that is automatically called when an instance of the class is created. It is used to initialize the object's properties and perform any setup required for the object.
+//     constructor(email:string, name:string ,){
+//         this.email=email;
+//         this.name=name
+//     }
+// }
 class User {
     constructor(email, name) {
-        this._courseCount = 1;
-        this.city = "";
-        this.name = name;
         this.email = email;
+        this.name = name;
+        this._countCourse = 1;
+        this.city = "harayana";
+    }
+    deletToken() {
+        console.log("deletToken");
     }
     get getAppleEmail() {
         return `apple${this.email}`;
     }
     get courseCount() {
-        return this._courseCount;
+        return this._countCourse;
     }
     set courseCount(courseNum) {
         if (courseNum <= 1) {
             throw new Error("Course count should be more than 1");
         }
-        this._courseCount = courseNum;
+        this._countCourse = courseNum;
     }
 }
 const gunit = new User("gunit.com", "gunit");
-gunit.city = "harayana";
+// gunit.city="harayana"
+// gunit.deletToken()
+// as this is not allowed as it is in priavte prop so it works only in the class of deletToken
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,51 @@
-class User{
+// class User {
 
-private _courseCount=1
+//     public email:string
+//     private name:string
+//     readonly city:string="harayana"      //In TypeScript, a constructor is a special method of a class that is automatically called when an instance of the class is created. It is used to initialize the object's properties and perform any setup required for the object.
+//     constructor(email:string, name:string ,){
+//         this.email=email;
+//         this.name=name
+//     }
+// }
 
-    email:string                                    //the type should be declared before the use as we are telling that this can be used in futures
-    name:string
-    city:string=""
-    constructor(email:string, name:string){
-        this.name=name
-        this.email=email
+class User {
 
-    }
+    private _countCourse=1
 
-    get getAppleEmail():string{
-    return`apple${this.email}`}
+    readonly city:string="harayana"  
+
+    constructor(
+        public email:string,
+        public name: string,
+        // private userId:string
+
+    ){
     
-    get courseCount():number
-    {
-    return this._courseCount}
-
-    set courseCount(courseNum: number){
-    if (courseNum <= 1){
-        throw new Error("Course count should be more than 1")
     }
-    this._courseCount = courseNum
-   }
+    private deletToken(){
+        console.log("deletToken")
+    }
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._countCourse
+    }
+
+    set courseCount (courseNum){          //there is not type       // in setter ther is nothuing in input type
+        if(courseNum <= 1){
+            throw new Error ("Course count should be more than 1")
+        }
+        this._countCourse=courseNum
+ 
+    }
+
 }
-const gunit=new User("gunit.com", "gunit")
-gunit.city="harayana"
 
+const gunit= new User ("gunit.com", "gunit")
+// gunit.city="harayana"
 
+// gunit.deletToken()
+// as this is not allowed as it is in priavte prop so it works only in the class of deletToken
