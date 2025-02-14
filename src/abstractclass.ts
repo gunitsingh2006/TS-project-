160d6b756dc8f13c21967886f876552040261b44
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-02-14 17:31:01 +0530

    Abstract Function Syntax

--- src/abstractclass.ts
+++ src/abstractclass.ts
@@ -2,13 +2,25 @@
  * Abstract base class.
  * Cannot be instantiated directly.
  */
-abstract class MyClass {
-    constructor() {
-        // constructor implementation  ->>>>>In TypeScript, a constructor is a special method of a class that is automatically called when an instance of the class is created. It is used to initialize the object's properties and perform any setup required for the object.
-    }
+abstract class TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string
+  ) {}
+}
 
-    /**
-     * Abstract method to be implemented by derived classes.
-     */
-    abstract myMethod(): void;
+class Instagram extends TakePhoto {  
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public brust: number
+  ) {
+    // constructor implementation  ->>>>>In TypeScript, a constructor is a special method of a class that is automatically called when an instance of the class is created. It is used to initialize the object's properties and perform any setup required for the object.
+
+    super(cameraMode, filter);
+  }
+
+  /**
+   * Abstract method to be implemented by derived classes.
+   */
 }
